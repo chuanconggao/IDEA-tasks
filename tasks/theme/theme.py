59099326f33240de2c6d5dcc10b88da5206ab8d0
Chuancong Gao <chuanconggao@users.noreply.github.com>
2016-06-15 11:15:46 -0700

    Update theme

--- tasks/theme/theme.py
+++ tasks/theme/theme.py
@@ -4,61 +4,55 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from collections import defaultdict, namedtuple
-import re
+import regex as re
 
-Node = namedtuple('Node', 'child, category')
-spliter = re.compile(r'^[^\w@#/+-]+|\W*\s+[^\w@#/+-]*')
+Node = namedtuple('Node', ['children', 'tags'])
+tokenizer = re.compile(r'^[^\w@#/+-]+|\W*\s+[^\w@#/+-]*', re.U)
 
 def build(themeDict):
     root = Node({}, set())
-    for theme, phrasess in themeDict.items():
-        for phrase in phrasess:
+    for tag, phrases in themeDict.iteritems():
+        for phrase in phrases:
+            words = [w for w in tokenizer.split(phrase) if len(w) > 0]
             node = root
-            for word in phrase:
-                if word not in node.child:
-                    node.child[word] = Node({}, set())
-                node = node.child[word]
-            node.category.add(theme)
+            for word in words:
+                node = node.children.setdefault(word, Node({}, set()))
+            node.tags.add(tag)
     return root
 
-def search(root, content, themes):
+def search(root, content, themeDict):
+    def _search(root, words):
+        matched = set()
+
+        for i in xrange(len(words)):
+            node = root
+            for word in words[i:]:
+                if word not in node.children:
+                    break
+
+                node = node.children[word]
+
+                matched |= node.tags
+
+        return matched
+
     counter = {
         t: {
-            "name": t,
             "total": 0,
-            "articles": defaultdict(int)
-        } for t in themes
+            "ranges": defaultdict(int)
+        } for t in themeDict
     }
-    for year, abstracts in content.items():
-        for abstract in abstracts:
-            matchThemes = _search(root, spliter.split(abstract))
-            for t in matchThemes:
+
+    for textRange, texts in content.iteritems():
+        for text in texts:
+            for t in _search(
+                    root,
+                    [w for w in tokenizer.split(text) if len(w) > 0]
+                ):
                 counter[t]["total"] += 1
-                counter[t]["articles"][year] += 1
+                counter[t]["ranges"][textRange] += 1
 
     return counter
-
-def _search(root, sentence):
-    matched = defaultdict(set)
-    startIndx = 0
-    while startIndx < len(sentence):
-        node = root
-        matchWords = []
-        for word in sentence[startIndx:]:
-            if word in node.child:
-                matchWords.append(word)
-                node = node.child[word]
-            else:
-                break
-
-        for category in node.category:
-            matched[category].add('.'.join(matchWords))
-        startIndx += 1
-    return matched
 
 def computeStat(idStr, content, themeDict):
-    trie = build(themeDict)
-
-    counter = search(trie, content, themeDict.keys())
-
-    return counter
+    return search(build(themeDict), content, themeDict)
